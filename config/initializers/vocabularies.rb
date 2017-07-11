f18fb4ffa497e8477e59b7138c24f0bea5efd8e5
Matt Barnett <matt@sixtyodd.com>
2017-07-11 10:06:08 -0600

    Add a bit of documentation

--- config/initializers/vocabularies.rb
+++ config/initializers/vocabularies.rb
@@ -1,3 +1,6 @@
+# Set up the app-wide +VOCABULARY+ hash from <tt>config/vocabularies.yml</tt>
+#
+# See <tt>config/vocabularies.yml</tt> for detailed descriptions of the file format and expected usage
 vocabs = {}
 
 config = YAML.safe_load(File.open(Rails.root.join('config', 'vocabularies.yml')))
