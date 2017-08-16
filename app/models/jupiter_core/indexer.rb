7fcab192aee7c060d346bd81baef79797571817e
Matt Barnett <matt@sixtyodd.com>
2017-08-16 14:43:29 -0600

    Fix an issue that cropped up in Ruby 2.4.1...
    related to https://bugs.ruby-lang.org/issues/12705

--- app/models/jupiter_core/indexer.rb
+++ app/models/jupiter_core/indexer.rb
@@ -5,7 +5,7 @@ class JupiterCore::Indexer < ActiveFedora::IndexingService
   def generate_solr_document
     super.tap do |solr_doc|
       object.owning_class.solr_calc_attributes.each do |name, metadata|
-        value = object.instance_eval(&metadata[:callable])
+        value = object.instance_exec(&metadata[:callable])
         Solrizer.insert_field(solr_doc, name, value, metadata[:type])
       end
     end
