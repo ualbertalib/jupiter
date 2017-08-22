1b30c31428a289964aa6a6fad0922664da76791a
Chris Want <cjwant@gmail.com>
2017-08-22 10:58:29 -0600

    Updated test of communities controller (no mocks).

--- test/controllers/communities_controller_test.rb
+++ test/controllers/communities_controller_test.rb
@@ -5,8 +5,11 @@ class CommunitiesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @admin = users(:admin)
     sign_in_as @admin
+
+    # TODO: setup proper fixtures for LockedLdpObjects
     @community = Community.new_locked_ldp_object(title: 'Nice book',
                                                  owner: @admin.id)
+    @community.unlock_and_fetch_ldp_object(&:save!)
   end
 
   test 'should get index' do
@@ -20,62 +23,35 @@ class CommunitiesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should create community' do
-    # This mocks the argument for the unlock_and_fetch_ldp_object block
-    ldp_object_mock = Minitest::Mock.new
-    ldp_object_mock.expect :save!, true
-
-    Community.stub :new_locked_ldp_object, @community do
-      @community.stub :unlock_and_fetch_ldp_object, true, ldp_object_mock do
-        @community.stub :to_param, '90210' do
-          post communities_url, params: { community: { title: 'Whatever' } }
-          assert_redirected_to community_url(@community)
-        end
-      end
+    assert_difference('Community.count') do
+      post communities_url, params: { community: { title: 'New Book' } }
     end
-    ldp_object_mock.verify
+
+    # TODO: implement a method to fetch most recently created, e.g. 'last'
+    # assert_redirected_to community_url(Community.last)
   end
 
   test 'should show community' do
-    Community.stub :find, @community do
-      @community.stub :to_param, '90210' do
-        get community_url(@community)
-        assert_response :success
-      end
-    end
+    get community_url(@community)
+    assert_response :success
   end
 
   test 'should get edit' do
-    Community.stub :find, @community do
-      @community.stub :to_param, '90210' do
-        get edit_community_url(@community)
-        assert_response :success
-      end
-    end
+    get edit_community_url(@community)
+    assert_response :success
   end
 
   test 'should update community' do
-    Community.stub :find, @community do
-      @community.stub :to_param, '90210' do
-        patch community_url(@community), params: { community: { title: 'Whatever' } }
-        assert_redirected_to community_url(@community)
-      end
-    end
+    patch community_url(@community), params: { community: { title: 'Updated Book' } }
+    assert_redirected_to community_url(@community)
   end
 
   test 'should destroy community' do
-    # This mocks the argument for the unlock_and_fetch_ldp_object block
-    ldp_object_mock = Minitest::Mock.new
-    ldp_object_mock.expect :destroy, true
-
-    Community.stub :find, @community do
-      @community.stub :to_param, '90210' do
-        @community.stub :unlock_and_fetch_ldp_object, true, ldp_object_mock do
-          delete community_url(@community)
-        end
-      end
+    assert_difference('Community.count', -1) do
+      delete community_url(@community)
     end
+
     assert_redirected_to admin_communities_and_collections_url
-    ldp_object_mock.verify
   end
 
 end
