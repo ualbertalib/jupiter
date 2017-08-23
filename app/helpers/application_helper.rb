bbe934da82374c45de0b33883f5d142a553c8eb6
Murny <murnagha@gmail.com>
2017-08-23 12:25:56 -0600

    Add page title helper and page title to all views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title(page_title)
+    base_title = 'ERA'
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
