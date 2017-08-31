2e6b4435926fc3d94b0c19471c30df8a71356c30
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-08-31 13:03:13 -0600

    remove trailing whitespace

--- config/puma.rb
+++ config/puma.rb
@@ -55,7 +55,6 @@ if ENV['RAILS_ENV'] == 'uat'
   workers ENV.fetch('WEB_CONCURRENCY') { 3 }
   preload_app!
   on_worker_boot do
-    
     ActiveSupport.on_load(:active_record) do
       ActiveRecord::Base.establish_connection
     end
