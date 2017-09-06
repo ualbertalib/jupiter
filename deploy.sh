a5fbee39d8522ed8a31b0cc81c958bc35ef9c255
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-09-06 15:18:25 -0600

    use docker-compose stop to stop the container properly

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-cd /home/deploy/jupiter && git pull origin master
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml rm -f -s -v
+cd /home/deploy/jupiter && git fetch --all && git reset --hard origin/master
+docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml stop web
 docker rmi $(docker images -f "dangling=true" -q)
 docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml build web
 docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml up -d
