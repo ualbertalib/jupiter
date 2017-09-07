094bb3c0bb00be8a56fd1fbbd4a9e9a24c3399b1
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-09-07 10:22:49 -0600

    added rake db:migrate and rake assets:precompile to the script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 cd /home/deploy/jupiter && git fetch --all && git reset --hard origin/master
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml stop web
+docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml down web
 docker rmi $(docker images -f "dangling=true" -q)
 docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml build web
 docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml up -d
+docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml run --rm web rake db:migrate
+#Recompile assets is optional, and can be commented out if not needed
+docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml run --rm web rake assets:precompile
