af96ed37cca9ef565278ce77d82ac788808f63f3
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-09-11 15:16:32 -0600

    updated path to the deployment folder

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-cd /home/deploy/jupiter && git fetch --all && git reset --hard origin/master
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml down web
+cd /home/deploy/jupiter-master && git fetch --all && git reset --hard origin/master
+docker-compose -f /home/deploy/jupiter-master/docker-compose.deployment.yml down web
 docker rmi $(docker images -f "dangling=true" -q)
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml pull web
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml up -d
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml run --rm web rake db:migrate
+docker-compose -f /home/deploy/jupiter-master/docker-compose.deployment.yml pull web
+docker-compose -f /home/deploy/jupiter-master/docker-compose.deployment.yml up -d
+docker-compose -f /home/deploy/jupiter-master/docker-compose.deployment.yml run --rm web rake db:migrate
 #Recompile assets is optional, and can be commented out if not needed
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml run --rm web rake assets:precompile
+docker-compose -f /home/deploy/jupiter-master/docker-compose.deployment.yml run --rm web rake assets:precompile
