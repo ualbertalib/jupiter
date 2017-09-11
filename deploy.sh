b80f00d90b0285e2d226bdfaf1c248acbb32e725
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-09-11 09:57:03 -0600

    updated docker-compose to pull the image from dockerhub's autobuild

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd /home/deploy/jupiter && git fetch --all && git reset --hard origin/master
 docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml down web
 docker rmi $(docker images -f "dangling=true" -q)
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml build web
+docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml pull web
 docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml up -d
 docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml run --rm web rake db:migrate
 #Recompile assets is optional, and can be commented out if not needed
