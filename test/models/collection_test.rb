c51efba1e1f7c77f9c43d3f398462f43f2ae91d1
Shane Murnaghan <murnagha@gmail.com>
2017-09-15 15:32:29 -0600

    Fix collections and communities forms (#141)
    * Fix collections and communities forms, so they render nice error messages
    
    * Improve collections tests
    
    * Improve community tests
    
    * Fix extra lines

--- test/models/collection_test.rb
+++ test/models/collection_test.rb
@@ -2,6 +2,12 @@ require 'test_helper'
 
 class CollectionTest < ActiveSupport::TestCase
 
+  test 'needs title' do
+    c = Collection.new_locked_ldp_object(owner: users(:admin).id)
+    refute c.valid?
+    assert_equal "Title can't be blank", c.errors.full_messages.first
+  end
+
   test 'visibility callback' do
     c = Collection.new_locked_ldp_object(title: 'foo', owner: users(:regular_user).id)
     assert c.valid?
