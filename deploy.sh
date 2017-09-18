1ee116781a02f3bdda8654963472fe02aff70a20
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-09-18 13:44:02 -0600

    updated deploy.sh for deployment docker on UAT

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker rmi $(docker images -f "dangling=true" -q)
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml pull web
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml up -d
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml run --rm web rake db:migrate
+docker-compose -f docker-compose.deployment.yml pull web
+docker-compose -f docker-compose.deployment.yml up -d
+docker-compose -f docker-compose.deployment.yml run --rm web rake db:migrate
 #Recompile assets is optional, and can be commented out if not needed
-docker-compose -f /home/deploy/jupiter/docker-compose.deployment.yml run --rm web rake assets:precompile
+docker-compose -f docker-compose.deployment.yml run --rm web rake assets:precompile
