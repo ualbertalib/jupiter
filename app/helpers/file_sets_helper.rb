7e9c04191ac6d5ec9b60b7eefb9ea423ee674cbf
Chris Want <cjwant@gmail.com>
2017-09-18 16:31:43 -0600

    Making the Item file view more closely match the wireframes.

--- app/helpers/file_sets_helper.rb
+++ app/helpers/file_sets_helper.rb
@@ -1,7 +1,6 @@
 module FileSetsHelper
-  def link_to_original_file(file_set)
-    url = url_for(controller: :file_sets, action: :download_original, id: file_set.id,
-                  file_name: file_set.original_file_name)
-    link_to(file_set.original_file_name, url).html_safe
+  def original_file_url(file_set)
+    url_for(controller: :file_sets, action: :download_original, id: file_set.id,
+            file_name: file_set.original_file_name)
   end
 end
