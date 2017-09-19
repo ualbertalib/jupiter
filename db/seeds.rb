52944e97e8ce7ddd242705979441735bc101120b
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-09-19 10:14:20 -0600

    allow seeding into current uat env

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-if Rails.env.development?
+if Rails.env.development? || Rails.env.uat?
   require 'active_fedora/cleaner'
   require "open-uri"
   require 'faker'
