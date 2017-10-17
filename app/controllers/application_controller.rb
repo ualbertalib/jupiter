d636d7188a273f686e145306abf51e8fd756bc4d
Chris Want <cjwant@gmail.com>
2017-10-17 15:28:45 -0600

    Switch user activity job to `perform_now` on sign-in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < ActionController::Base
   def sign_in(user)
     @current_user = user
     session[:user_id] = user.try(:id)
-    UpdateUserActivityJob.perform_later(@current_user.id, Time.now.utc.to_s, request.remote_ip, sign_in: true)
+    UpdateUserActivityJob.perform_now(@current_user.id, Time.now.utc.to_s, request.remote_ip, sign_in: true)
   end
 
   # Logs out the current user.
