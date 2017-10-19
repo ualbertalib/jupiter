8f9dac21c49115594375224c6f2890ff813f612f
Chris Want <cjwant@gmail.com>
2017-10-19 13:46:54 -0600

    Two things:
    * Move the filter select items to a helper
    * Don't raise error on invalid filter choice

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ApplicationRecord
 
-  FILTER_MAP = { all: I18n.t(:all),
-                 admin: I18n.t('admin.users.admin_role'),
-                 user: I18n.t('admin.users.user_role'),
-                 suspended: I18n.t('admin.users.suspended_status'),
-                 active: I18n.t('admin.users.active_status') }.freeze
-
   has_many :identities, dependent: :destroy
   has_many :site_notifications, dependent: :destroy
 
@@ -33,10 +27,8 @@ class User < ApplicationRecord
         where(suspended: false)
       when 'suspended'
         where(suspended: true)
-      else
-        # we just ignore 'all' (means unscoped), fail on everything else
-        raise ArgumentError, 'not a valid filter' unless FILTER_MAP.keys.include?(filter.to_sym)
       end
+      # we just ignore everything else (including 'all' -- means unscoped)
     end
   }
 
