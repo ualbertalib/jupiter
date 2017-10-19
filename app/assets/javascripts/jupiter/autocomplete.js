803ba284e634330eedcf6ef59f26ac5d62324056
Chris Want <cjwant@gmail.com>
2017-10-19 14:45:55 -0600

    Remove JS AJAX calls for 'remote-true' links.

--- app/assets/javascripts/jupiter/autocomplete.js
+++ app/assets/javascripts/jupiter/autocomplete.js
@@ -1,9 +1,4 @@
 $(document).on('turbolinks:load', function() {
-  // Fetch new table when clicking headers and pagination links
-  $('.jupiter-autocomplete-results').on('click', 'thead a, a.page-link', function() {
-    $.getScript(this.href);
-    return false;
-  });
   // Fetch new table for autocomplete and filter widgets
   $('.jupiter-autocomplete input,select').bind("change keyup input search", function() {
     render_new_table($(this));
