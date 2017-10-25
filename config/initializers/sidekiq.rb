064fadb28e1b5a038199f2877fec7f293c67394e
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-10-25 14:53:25 -0600

    update sidekiq config file so it can use secrets.yml

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
+require 'yaml'
+require 'erb'
+secrets = YAML.load_file("config/secrets.yml")
+redis_url = ERB.new(secrets["shared"]["redis_url"]).result(binding)
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: redis_url }
 end
