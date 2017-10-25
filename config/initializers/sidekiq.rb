5670041208a11eede0af6bb3043d0cc44e63cc05
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-10-25 14:56:25 -0600

    address other PR comments

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 require 'erb'
-secrets = YAML.load_file("config/secrets.yml")
-redis_url = ERB.new(secrets["shared"]["redis_url"]).result(binding)
+secrets = YAML.load_file('config/secrets.yml')
+redis_url = ERB.new(secrets['shared']['redis_url']).result(binding)
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url }
 end
