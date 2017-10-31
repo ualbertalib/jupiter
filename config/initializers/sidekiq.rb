6dceeb6e6da3fc1e3bfd7e500a5c0765d6045fb3
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-10-31 16:35:13 -0600

    Use secrets.yml to manage env var in deployment (#218)
    update configuration files to make all env vars managed by secrets.yml

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: Rails.application.secrets.redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: Rails.application.secrets.redis_url }
 end
