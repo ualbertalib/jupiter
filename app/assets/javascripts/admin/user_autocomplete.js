3e0df2b542832ff8b057954892bf8fd439f2c58d
Murny <murnagha@gmail.com>
2017-11-01 11:04:55 -0600

    Fix event bindings, was spamming server with requests

--- app/assets/javascripts/admin/user_autocomplete.js
+++ app/assets/javascripts/admin/user_autocomplete.js
@@ -1,9 +1,16 @@
 $(document).on('turbolinks:load', function() {
   // Fetch new table for autocomplete and filter widgets
-  $('.js-user-autocomplete input,select').bind("change keyup input search", function() {
+  $('.js-user-autocomplete select').change(function() {
     render_new_table($(this));
     return false;
   });
+
+  // TODO: Could be improved. Should maybe bring in a `debounce` function for this search
+  $('.js-user-autocomplete input').keyup(function() {
+    render_new_table($(this));
+    return false;
+  });
+
 
   function render_new_table($changed_element) {
     var $form = $changed_element.closest('form');
