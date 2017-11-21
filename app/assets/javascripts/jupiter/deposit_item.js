35ba8d21ffdf900e83736dfce3dcbcba0dc2b12b
Murny <murnagha@gmail.com>
2017-11-21 15:59:29 -0700

    Add javascript for unsaved page dialog and expanding accordions

--- app/assets/javascripts/jupiter/deposit_item.js
+++ app/assets/javascripts/jupiter/deposit_item.js
@@ -1,12 +1,42 @@
+var unsavedChanges = false;
+
 $(document).on('turbolinks:load', function() {
+  unsavedChanges = false;
 
   function toggleIcon(e) {
     $(e.target)
       .prev('.card-header')
       .find('.js-more-less')
-      .toggleClass('fa-plus fa-minus');
+      .toggleClass('fa-chevron-down fa-chevron-up');
   }
-  $('#license-accordion .card').on('hidden.bs.collapse', toggleIcon);
-  $('#license-accordion .card').on('shown.bs.collapse', toggleIcon);
 
+  $('#js-license-accordion .card').on('hidden.bs.collapse', toggleIcon);
+  $('#js-license-accordion .card').on('shown.bs.collapse', toggleIcon);
+
+  $('#js-additional-fields-accordion .card').on('hidden.bs.collapse', toggleIcon);
+  $('#js-additional-fields-accordion .card').on('shown.bs.collapse', toggleIcon);
+
+
+  $('form.js-deposit-item input').change(function() {
+    return unsavedChanges = true;
+  });
+
+  $('form.js-deposit-item').submit(function() {
+    return unsavedChanges = false;
+  });
+
+});
+
+$(document).on('turbolinks:before-visit', function() {
+  if (unsavedChanges) {
+    return confirm("Any changes you have made will NOT be saved. Are you sure you want to leave?");
+  }
+});
+
+$(window).bind('beforeunload', function(event) {
+  if (unsavedChanges) {
+    var msg = "Any changes you have made will NOT be saved. Are you sure you want to leave?";
+    event.returnValue = msg;
+    return msg;
+  }
 });
