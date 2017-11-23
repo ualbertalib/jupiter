a42734629c10d37c50b759c288f978396b1c01d0
Murny <murnagha@gmail.com>
2017-11-23 15:26:27 -0700

    Generalize select2 intializer by using data-attrs, add placeholders for everything, take an another attempt at width bug with select2

--- app/assets/javascripts/jupiter/deposit_item.js
+++ app/assets/javascripts/jupiter/deposit_item.js
@@ -28,34 +28,35 @@ $(document).on('turbolinks:load', function() {
   // bring over community/collection select from items (tweaked a bit)
   // (we only need to handle one pairing for time being)
   $('form.js-deposit-item .js-community-select').change(function() {
+    var $collectionSelect = $('.js-collection-select')
     var id =  $(this).find('option:selected').val();
     if (!id) {
-      $('.js-collection-select').prop('disabled', true).empty();
+      $collectionSelect.prop('disabled', true).empty();
     } else {
       $.getJSON('/communities/' + id + '.json').done(function(data) {
-        var items = "<option value>Select a collection</option>";
-
+        var items = '<option value>' + $collectionSelect.data('placeholder') + '</option>';
         $.each(data.collections, function(idx, item) {
           items += '<option value="' + item.id + '">' + item.title + '</option>';
         });
-        $('.js-collection-select').prop('disabled', false)
+        $collectionSelect.prop('disabled', false)
                                   .empty().append(items);
       });
     }
   })
 
-  // select2 initailizations
-  $('.js-select2-languages').select2({
+  // global select2 initailization (could be moved elsewhere)
+  // We going to make heavy use of data-attrs to customize this instead
+  $('.js-select2').select2({
     theme: 'bootstrap',
-    allowClear: true
+    allowClear: true,
+
+    // TODO: Hack on width which fixes mobile responsiveness width and select2 inputs being
+    // a larger width then normal bootstrap inputs
+    // See deposit_item.scss for similar comments.
+    // Potential fix here: https://github.com/select2/select2/pull/4898/
+    width: '100%'
   });
 
-  $('.js-select2-tags').select2({
-    theme: 'bootstrap',
-    placeholder: 'Enter multiple values',
-    tags: true,
-    allowClear: true
-  });
 });
 
 $(document).on('turbolinks:before-visit', function() {
