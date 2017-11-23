4867b5a6ea13b6897fcd0489489a33d7c8ed8721
Chris Want <cjwant@gmail.com>
2017-11-23 11:28:08 -0700

    Run select2 directly on selector, don't iterate.

--- app/assets/javascripts/jupiter/communities_collections_searchbox.js
+++ app/assets/javascripts/jupiter/communities_collections_searchbox.js
@@ -1,25 +1,23 @@
 $(document).on('turbolinks:load', function() {
-  $('.js-communities-collections-searchbox').each(function() {
-    $(this).select2({
-      theme: 'bootstrap',
-      ajax: {
-        url: 'communities',
-        cache: false,
-        dataType: 'json',
-        data: function(params) {
-          var query = {
-            query: params.term
-          };
-          return query;
-        }
+  $('.js-communities-collections-searchbox').select2({
+    theme: 'bootstrap',
+    ajax: {
+      url: 'communities',
+      cache: false,
+      dataType: 'json',
+      data: function(params) {
+        var query = {
+          query: params.term
+        };
+        return query;
       }
-    }).on('select2:select', function(e) {
-      var data = e.params.data;
-      if (data.path) {
-        // To ensure we get the placeholder text when user clicks selection then back button (firefox)
-        $(this).val(null).trigger("change");
-        window.location.href = data.path;
-      }
-    });
+    }
+  }).on('select2:select', function(e) {
+    var data = e.params.data;
+    if (data.path) {
+      // To ensure we get the placeholder text when user clicks selection then back button (firefox)
+      $(this).val(null).trigger("change");
+      window.location.href = data.path;
+    }
   });
 });
