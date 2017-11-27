dade25089b6679e9c8d7129433a2f1fe7d9f9d14
Murny <murnagha@gmail.com>
2017-11-27 16:24:33 -0700

    Get dropzone actually submitting in correct sorted order

--- app/assets/javascripts/jupiter/dropzone.js
+++ app/assets/javascripts/jupiter/dropzone.js
@@ -3,7 +3,7 @@ Dropzone.autoDiscover = false;
 
 $(document).on('turbolinks:load', function() {
 
-  // Get the template HTML and remove it from the doument
+  // Get the template HTML and remove it from the document
   var previewNode = document.querySelector("#template");
   previewNode.id = "";
   var previewTemplate = previewNode.parentNode.innerHTML;
@@ -11,90 +11,68 @@ $(document).on('turbolinks:load', function() {
 
 
   var myDropzone = new Dropzone('form.js-dropzone', {
-    paramName: 'files',
+    paramName: 'item[files]',
     thumbnailWidth: 80,
     thumbnailHeight: 80,
-    parallelUploads: 20,
+    parallelUploads: 100,
+    maxFiles: 100,
     previewTemplate: previewTemplate,
-    autoQueue: false, // Make sure the files aren't queued until manually added
+    autoProcessQueue: false,
+    uploadMultiple: true,
     previewsContainer: "#previews", // Define the container to display the previews
-    clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+    clickable: ".js-add-files", // Define the element that should be used as click trigger to select files.
+
+    // The setting up of the dropzone
+    init: function() {
+      var myDropzone = this;
+
+      // First change the button to actually tell Dropzone to process the queue.
+      this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
+        // Make sure that the form isn't actually being sent.
+        e.preventDefault();
+        e.stopPropagation();
+        myDropzone.processQueue();
+      });
+
+      // Listen to the sendingmultiple event. In this case, it's the sendingmultiple event instead
+      // of the sending event because uploadMultiple is set to true.
+      this.on("sendingmultiple", function() {
+        // Gets triggered when the form is actually being sent.
+        // Hide the success button or the complete form.
+      });
+      this.on("successmultiple", function(files, response) {
+        // Gets triggered when the files have successfully been sent.
+        // Redirect user or notify of success.
+      });
+      this.on("errormultiple", function(files, response) {
+        // Gets triggered when there was an error sending the files.
+        // Maybe show form again, and notify user of error
+      });
+    }
+
   });
 
-  var el = document.querySelector('.files');
-  var sortable = Sortable.create(el);
-
-  myDropzone.on("addedfile", function(file) {
-    // Hookup the start button
-    file.previewElement.querySelector(".start").onclick = function() {
-      myDropzone.enqueueFile(file);
-    };
-  });
-
-  // Update the total progress bar
-  myDropzone.on("totaluploadprogress", function(progress) {
-    document.querySelector("#total-progress .progress-bar").style.width = progress + "%";
-  });
-
-  myDropzone.on("sending", function(file) {
-    // Show the total progress bar when upload starts
-    document.querySelector("#total-progress").style.opacity = "1";
-    // And disable the start button
-    file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
-  });
-
-  // Hide the total progress bar when nothing's uploading anymore
-  myDropzone.on("queuecomplete", function(progress) {
-    document.querySelector("#total-progress").style.opacity = "0";
-  });
-
-  // Setup the buttons for all transfers
-  // The "add files" button doesn't need to be setup because the config
-  // `clickable` has already been specified.
-  document.querySelector(".js-dropzone .start").onclick = function() {
-    myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
-  };
-  document.querySelector(".js-dropzone .cancel").onclick = function() {
+  document.querySelector(".js-dropzone .js-cancel-files").onclick = function() {
     myDropzone.removeAllFiles(true);
   };
 
-
-  // Now fake the file upload, since GitHub does not handle file uploads
-  // and returns a 404
-
-  var minSteps = 6,
-      maxSteps = 60,
-      timeBetweenSteps = 100,
-      bytesPerStep = 100000;
-
-  myDropzone.uploadFiles = function(files) {
-    var self = this;
-
-    for (var i = 0; i < files.length; i++) {
-
-      var file = files[i];
-      totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
-
-      for (var step = 0; step < totalSteps; step++) {
-        var duration = timeBetweenSteps * (step + 1);
-        setTimeout(function(file, totalSteps, step) {
-          return function() {
-            file.upload = {
-              progress: 100 * (step + 1) / totalSteps,
-              total: file.size,
-              bytesSent: (step + 1) * file.size / totalSteps
-            };
-
-            self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
-            if (file.upload.progress == 100) {
-              file.status = Dropzone.SUCCESS;
-              self.emit("success", file, 'success', null);
-              self.emit("complete", file);
-              self.processQueue();
-            }
-          };
-        }(file, totalSteps, step), duration);
-      }
-    }
-  }
+  // Make files previews sortable
+  var el = document.querySelector('.js-files');
+  var sortable = Sortable.create(el, {
+	  // Called by any change to the list (add / update / remove)
+    onSort: function (/**Event*/evt) {
+      // Get the queued files
+      var files = myDropzone.getQueuedFiles();
+      // Sort theme based on the DOM element index
+      files.sort(function(a, b){
+          return ($(a.previewElement).index() > $(b.previewElement).index()) ? 1 : -1;
+      })
+      // Clear the dropzone queue
+      myDropzone.removeAllFiles();
+      // Add the reordered files to the queue
+      myDropzone.handleFiles(files);
+	  },
+  });
 });
+
+
