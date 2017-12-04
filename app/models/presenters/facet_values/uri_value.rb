c03b924aa7a0bf77b61da24c15aa798d969a930e
Matt Barnett <matt@sixtyodd.com>
2017-12-04 14:54:47 -0700

    Improve arg validation

--- app/models/presenters/facet_values/uri_value.rb
+++ app/models/presenters/facet_values/uri_value.rb
@@ -4,12 +4,13 @@ class Presenters::FacetValues::URIValue < Presenters::FacetValues::DefaultPresen
 
   def translate_uri(vocab, uri)
     raise ArgumentError unless vocab.is_a? Symbol
+    raise ArgumentError, "Vocabulary not found: #{vocab}" unless CONTROLLED_VOCABULARIES.key?(vocab)
     CONTROLLED_VOCABULARIES[vocab].each do |entry|
       if entry[:uri] == uri
         return I18n.t("controlled_vocabularies.#{vocab}.#{entry[:code]}")
       end
     end
-    raise ArgumentError, "Vocabulary not found: #{vocab}"
+    raise ArgumentError, "URI: #{uri} not found in vocabulary: #{vocab}"
   end
 
 end
