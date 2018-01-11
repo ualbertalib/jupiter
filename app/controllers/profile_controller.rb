a3e6669f5b51d3a8977fc713eca0e930c2cbdac0
Chris Want <cjwant@gmail.com>
2018-01-11 11:14:57 -0700

    Remove `before_action` from ProfileController to reduce code.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,17 +2,10 @@ class ProfileController < ApplicationController
 
   include ItemSearch
 
-  before_action :ensure_logged_in
-
   def index
+    authorize :user, :logged_in?
     @user = current_user
     item_search_setup(Item.search_term_for(:owner, @user.id, role: :exact_match))
-  end
-
-  private
-
-  def ensure_logged_in
-    authorize :user, :logged_in?
   end
 
 end
