594cef751950dbcaa84394611ec429814e41b62e
Matt Barnett <matt@sixtyodd.com>
2018-02-08 12:49:12 -0700

    Fix a weird test issue

--- test/system/item_show_test.rb
+++ test/system/item_show_test.rb
@@ -4,6 +4,7 @@ class ItemShowTest < ApplicationSystemTestCase
 
   def before_all
     super
+    @user = User.find_by(email: 'john_snow@example.com')
     @community = Community.new_locked_ldp_object(title: 'Fancy Community', owner: 1)
                           .unlock_and_fetch_ldp_object(&:save!)
     @collection = Collection.new_locked_ldp_object(community_id: @community.id,
@@ -11,16 +12,16 @@ class ItemShowTest < ApplicationSystemTestCase
                             .unlock_and_fetch_ldp_object(&:save!)
 
     # Half items have 'Fancy' in title, others have 'Nice', distributed between the two collections
-    @item1 = Item.new_locked_ldp_object(visibility: JupiterCore::VISIBILITY_PUBLIC,
-                                        owner: users(:regular).id, title: 'Fancy Item',
-                                        creators: ['Joe Blow'],
-                                        created: '2011-11-11',
-                                        languages: [CONTROLLED_VOCABULARIES[:language].english],
-                                        license: CONTROLLED_VOCABULARIES[:license].attribution_4_0_international,
-                                        item_type: CONTROLLED_VOCABULARIES[:item_type].article,
-                                        publication_status: [CONTROLLED_VOCABULARIES[:publication_status].published],
-                                        subject: ['Fancy things'])
-                 .unlock_and_fetch_ldp_object do |uo|
+    @item = Item.new_locked_ldp_object(visibility: JupiterCore::VISIBILITY_PUBLIC,
+                                       owner: @user.id, title: 'Fancy Item',
+                                       creators: ['Joe Blow'],
+                                       created: '2011-11-11',
+                                       languages: [CONTROLLED_VOCABULARIES[:language].english],
+                                       license: CONTROLLED_VOCABULARIES[:license].attribution_4_0_international,
+                                       item_type: CONTROLLED_VOCABULARIES[:item_type].article,
+                                       publication_status: [CONTROLLED_VOCABULARIES[:publication_status].published],
+                                       subject: ['Fancy things'])
+                .unlock_and_fetch_ldp_object do |uo|
       uo.add_to_path(@community.id, @collection.id)
       uo.add_to_path(@community.id, @collection.id)
       uo.save!
@@ -31,33 +32,34 @@ class ItemShowTest < ApplicationSystemTestCase
         end
       end
     end
+
     @item2 = Item.new_locked_ldp_object(visibility: JupiterCore::VISIBILITY_AUTHENTICATED,
-                                        owner: users(:regular).id, title: 'CCID Item',
-                                        creators: ['Joe Blow'],
-                                        created: '2011-11-11',
-                                        languages: [CONTROLLED_VOCABULARIES[:language].english],
-                                        license: CONTROLLED_VOCABULARIES[:license].attribution_4_0_international,
-                                        item_type: CONTROLLED_VOCABULARIES[:item_type].article,
-                                        publication_status: [CONTROLLED_VOCABULARIES[:publication_status].published],
-                                        subject: ['Fancy things'])
-                 .unlock_and_fetch_ldp_object do |uo|
+                                       owner: @user.id, title: 'CCID Item',
+                                       creators: ['Joe Blow'],
+                                       created: '2011-11-11',
+                                       languages: [CONTROLLED_VOCABULARIES[:language].english],
+                                       license: CONTROLLED_VOCABULARIES[:license].attribution_4_0_international,
+                                       item_type: CONTROLLED_VOCABULARIES[:item_type].article,
+                                       publication_status: [CONTROLLED_VOCABULARIES[:publication_status].published],
+                                       subject: ['Fancy things'])
+                .unlock_and_fetch_ldp_object do |uo|
       uo.add_to_path(@community.id, @collection.id)
       uo.add_to_path(@community.id, @collection.id)
       uo.save!
     end
   end
 
-  test 'Users should be able to download public files en-masse' do
-    visit item_path @item1
+  test 'unauthed users should be able to download all files from a public item' do
+    visit item_path @item
     assert_selector '.js-download', count: 2
     assert_selector '.js-download-all'
     # TODO: test that the files are downloaded via js successfully without making the suite brittle
   end
 
   test 'Search faceting on item values is not broken' do
-    visit item_path @item1
+    visit item_path @item
     click_link 'Joe Blow'
-    assert_selector "[href='/search?tab=item']", count: 1
+    assert_selector "a[href='/search?tab=item']", count: 1
   end
 
   test 'Visiting authenticated items as an unauthenticated user works' do
