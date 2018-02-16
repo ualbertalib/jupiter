cb32d71ad6448364eff7b5ebbca9d42dc70ff781
Chris Want <cjwant@gmail.com>
2018-02-16 16:09:23 -0700

    Issue #499, Admin Item search.
    Also:
    
    * refactor item search/results view for reuse
    * Make item count on dashboard include theses.

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,10 @@
 class Admin::ItemsController < Admin::AdminController
 
+  include ItemSearch
+
   def index
     @items = Item.limit(10).sort(:record_created_at, :desc)
+    item_search_setup
   end
 
 end
