8de8c7c1f32db0ae33efe094e904500a3ad6b9e4
Matthew Barnett <matt@sixtyodd.com>
2018-02-16 09:28:09 -0700

    Separate concerns between RDF term URI naming and Ruby method names (#480)
    and make all-lowercase -> camel case

--- app/models/concerns/object_properties.rb
+++ app/models/concerns/object_properties.rb
@@ -6,7 +6,7 @@ module ObjectProperties
     has_attribute :title, ::RDF::Vocab::DC.title, solrize_for: [:search, :sort]
 
     # UAL attributes
-    has_attribute :fedora3_uuid, ::TERMS[:ual].fedora3uuid, solrize_for: :exact_match
+    has_attribute :fedora3_uuid, ::TERMS[:ual].fedora3_uuid, solrize_for: :exact_match
     has_attribute :depositor, ::TERMS[:ual].depositor, solrize_for: [:search]
 
     unlocked do
