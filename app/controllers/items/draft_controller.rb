c041b8d5fcb7854e517e8ae9722d59156e237dea
Murny <murnagha@gmail.com>
2018-03-27 13:10:23 -0600

    Fix cops from merging master in

--- app/controllers/items/draft_controller.rb
+++ app/controllers/items/draft_controller.rb
@@ -54,13 +54,9 @@ class Items::DraftController < ApplicationController
       # TODO: Handle required year but optional day/month better? Keep as string?
       # Set month/day to Jan 1st if left blank
       if params[:draft_item][:date_created].blank?
-        if params[:draft_item]['date_created(3i)'].blank?
-          params[:draft_item]['date_created(3i)'] = '1'
-        end
+        params[:draft_item]['date_created(3i)'] = '1' if params[:draft_item]['date_created(3i)'].blank?
 
-        if params[:draft_item]['date_created(2i)'].blank?
-          params[:draft_item]['date_created(2i)'] = '1'
-        end
+        params[:draft_item]['date_created(2i)'] = '1' if params[:draft_item]['date_created(2i)'].blank?
       end
 
       @draft_item.update(permitted_attributes(DraftItem))
