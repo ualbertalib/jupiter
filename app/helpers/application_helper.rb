f57000539344e4bf8648d2f27fd2a50fd9f30c69
Murny <murnagha@gmail.com>
2018-04-05 14:23:15 -0600

    Create page description helper and refactor meta descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,34 @@ module ApplicationHelper
     @page_title.join(' | ')
   end
 
+  # Define or get a description for the current page
+  #
+  # description - String (default: nil)
+  #
+  # If this helper is called multiple times with an argument, only the last
+  # description will be returned when called without an argument. Descriptions
+  # have newlines replaced with spaces and all HTML tags are sanitized.
+  #
+  # Examples:
+  #
+  #   page_description # => "Default Jupiter Welcome Lead"
+  #   page_description("Foo")
+  #   page_description # => "Foo"
+  #
+  #   page_description("<b>Bar</b>\nBaz")
+  #   page_description # => "Bar Baz"
+  #
+  # Returns an HTML-safe String.
+  def page_description(description = nil)
+    if description.present?
+      @page_description = description.squish
+    elsif @page_description.present?
+      jupiter_truncate(sanitize(@page_description), length: 140)
+    else
+      @page_description = t('welcome.index.welcome_lead')
+    end
+  end
+
   def humanize_uri_code(vocab, code)
     t("controlled_vocabularies.#{vocab}.#{code}")
   end
