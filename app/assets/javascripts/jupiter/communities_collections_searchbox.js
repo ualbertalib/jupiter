9a9fdc199078084549a545692e3e730389e7a1cb
Murny <murnagha@gmail.com>
2018-04-10 15:59:00 -0600

    Fix showing all results instead of limiting to 4 and fix responsiveness

--- app/assets/javascripts/jupiter/communities_collections_searchbox.js
+++ app/assets/javascripts/jupiter/communities_collections_searchbox.js
@@ -1,30 +1,36 @@
 $(document).on('turbolinks:load', function() {
   $input = $(".js-communities-collections-searchbox");
 
-  var options = {
-    getValue: "name",
-    url: function(phrase) {
-      // we use a relative url "communities.json" here as this will be used for both /admin and non admin (/) paths
-      return "communities.json?query=" + phrase;
-    },
-    categories: [
-      {
-        listLocation: "communities",
-        header: "<strong>Communities</strong>",
+  if($input.length > 0) {
+    var options = {
+      adjustWidth: false,
+      getValue: "name",
+      url: function(phrase) {
+        // we use a relative url "communities.json" here as this will be used for both /admin and non admin (/) paths
+        return "communities.json?query=" + phrase;
       },
-      {
-        listLocation: "collections",
-        header: "<strong>Collections</strong>",
+      categories: [
+        {
+          maxNumberOfElements:5,
+          listLocation: "communities",
+          header: "<strong>Communities</strong>",
+        },
+        {
+          maxNumberOfElements:5,
+          listLocation: "collections",
+          header: "<strong>Collections</strong>",
+        }
+      ],
+      list: {
+        onChooseEvent: function() {
+          var url = $input.getSelectedItemData().url;
+          $input.val("");
+          Turbolinks.visit(url);
+        }
       }
-    ],
-    list: {
-      onChooseEvent: function() {
-        var url = $input.getSelectedItemData().url;
-        $input.val("");
-        Turbolinks.visit(url);
-      }
-    }
-  };
+    };
 
-  $input.easyAutocomplete(options);
+    $input.easyAutocomplete(options);
+  }
+
 });
