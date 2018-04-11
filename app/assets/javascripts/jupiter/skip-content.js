187d18566207c998877ce53dc2d2b26201645294
Murny <murnagha@gmail.com>
2018-04-11 13:30:17 -0600

    Generalize javascript more so can be reused

--- app/assets/javascripts/jupiter/skip-content.js
+++ app/assets/javascripts/jupiter/skip-content.js
@@ -1,11 +1,6 @@
 $(document).on('turbolinks:load', function() {
   $(".js-skip-to-content").click(function (event) {
-    $('#content').attr('tabIndex', -1).focus();
-    event.preventDefault();
-  });
-
-  $(".js-skip-to-results").click(function (event) {
-    $('#results').attr('tabIndex', -1).focus();
+    $($(this).attr('href')).attr('tabIndex', -1).focus();
     event.preventDefault();
   });
 });
