d034652965fc4a5be07d7c85f69ddec9552eba89
Matt Barnett <matt@sixtyodd.com>
2018-08-01 15:43:48 -0600

    Test search controller ordering

--- Gemfile
+++ Gemfile
@@ -112,6 +112,7 @@ group :test do
   gem 'vcr', require: false
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'rails-controller-testing'
   gem 'webmock', require: false
 end
 
---
+++ test/controllers/search_controller_test.rb
@@ -0,0 +1,91 @@
+require 'test_helper'
+
+class SearchControllerTest < ActionDispatch::IntegrationTest
+
+  def before_all
+    super
+    @community = Community.new_locked_ldp_object(title: 'Community',
+                                                 owner: 1)
+    @community.unlock_and_fetch_ldp_object(&:save!)
+    @collection = Collection.new_locked_ldp_object(community_id: @community.id,
+                                                   title: 'Collection', owner: 1).unlock_and_fetch_ldp_object(&:save!)
+
+    @item1 = Item.new_locked_ldp_object(visibility: JupiterCore::VISIBILITY_PUBLIC,
+                                        owner: 1, title: 'Ant',
+                                        creators: ['Joe Blow'],
+                                        created: '1000000 BC',
+                                        languages: [CONTROLLED_VOCABULARIES[:language].english],
+                                        item_type: CONTROLLED_VOCABULARIES[:item_type].article,
+                                        publication_status: [CONTROLLED_VOCABULARIES[:publication_status].published],
+                                        license: CONTROLLED_VOCABULARIES[:license].attribution_4_0_international,
+                                        subject: ['Items']).unlock_and_fetch_ldp_object do |uo|
+      uo.add_to_path(@community.id, @collection.id)
+      uo.save!
+    end
+
+    @item2 = Item.new_locked_ldp_object(visibility: JupiterCore::VISIBILITY_PUBLIC,
+                                        owner: 1, title: 'Moose',
+                                        creators: ['Joe Blow'],
+                                        created: '1000000 BC',
+                                        languages: [CONTROLLED_VOCABULARIES[:language].english],
+                                        item_type: CONTROLLED_VOCABULARIES[:item_type].article,
+                                        publication_status: [CONTROLLED_VOCABULARIES[:publication_status].published],
+                                        license: CONTROLLED_VOCABULARIES[:license].attribution_4_0_international,
+                                        subject: ['Items']).unlock_and_fetch_ldp_object do |uo|
+      uo.add_to_path(@community.id, @collection.id)
+      uo.save!
+    end
+
+    @item3 = Item.new_locked_ldp_object(visibility: JupiterCore::VISIBILITY_PUBLIC,
+                                        owner: 1, title: 'Zebra',
+                                        creators: ['Joe Blow'],
+                                        created: '1000000 BC',
+                                        languages: [CONTROLLED_VOCABULARIES[:language].english],
+                                        item_type: CONTROLLED_VOCABULARIES[:item_type].article,
+                                        publication_status: [CONTROLLED_VOCABULARIES[:publication_status].published],
+                                        license: CONTROLLED_VOCABULARIES[:license].attribution_4_0_international,
+                                        subject: ['Items']).unlock_and_fetch_ldp_object do |uo|
+      uo.add_to_path(@community.id, @collection.id)
+      uo.save!
+    end
+
+    @item4 = Item.new_locked_ldp_object(visibility: JupiterCore::VISIBILITY_PUBLIC,
+                                        owner: 1, title: 'Ant Moose',
+                                        creators: ['Joe Blow'],
+                                        created: '1000000 BC',
+                                        languages: [CONTROLLED_VOCABULARIES[:language].english],
+                                        item_type: CONTROLLED_VOCABULARIES[:item_type].article,
+                                        publication_status: [CONTROLLED_VOCABULARIES[:publication_status].published],
+                                        license: CONTROLLED_VOCABULARIES[:license].attribution_4_0_international,
+                                        subject: ['Items']).unlock_and_fetch_ldp_object do |uo|
+      uo.add_to_path(@community.id, @collection.id)
+      uo.save!
+    end
+
+    @item5 = Item.new_locked_ldp_object(visibility: JupiterCore::VISIBILITY_PUBLIC,
+                                        owner: 1, title: 'Moose Ant',
+                                        creators: ['Joe Blow'],
+                                        created: '1000000 BC',
+                                        languages: [CONTROLLED_VOCABULARIES[:language].english],
+                                        item_type: CONTROLLED_VOCABULARIES[:item_type].article,
+                                        publication_status: [CONTROLLED_VOCABULARIES[:publication_status].published],
+                                        license: CONTROLLED_VOCABULARIES[:license].attribution_4_0_international,
+                                        subject: ['Items']).unlock_and_fetch_ldp_object do |uo|
+      uo.add_to_path(@community.id, @collection.id)
+      uo.save!
+    end
+  end
+
+  test 'should get results in alphabetical order when no query present' do
+    get search_url
+    assert_response :success
+    assert_equal [@item1.id, @item4.id, @item2.id, @item5.id, @item3.id], assigns[:results].to_a.map(&:id)
+  end
+
+  test 'should get results in relevance order when a query is present' do
+    get search_url, params: { search: 'Moose' }
+    assert_response :success
+    assert_equal [@item2.id, @item5.id, @item4.id], assigns[:results].to_a.map(&:id)
+  end
+
+end
