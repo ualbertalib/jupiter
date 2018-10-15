9272ec6cf822c1f622910793ef4dc40670f8c91c
Weiwei Shi <weiwei.shi@ualberta.ca>
2018-10-15 10:21:00 -0600

    allow env var to manage where/if scheduled jobs need to be run

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,9 @@ Sidekiq.configure_server do |config|
   config.redis = { url: Rails.application.secrets.redis_url }
 
   schedule_file = 'config/schedule.yml'
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
+  if File.exist?(schedule_file) && Rails.application.secrets.run_scheduled_jobs
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) 
+  end
 end
 
 Sidekiq.configure_client do |config|
