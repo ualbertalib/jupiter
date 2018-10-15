59e5eef31480bbfd8a0d944aa50a26aac38c3458
Weiwei Shi <weiwei.shi@ualberta.ca>
2018-10-15 10:00:54 -0600

    use the optimized query to fix #888

--- app/jobs/garbage_collect_blobs_job.rb
+++ app/jobs/garbage_collect_blobs_job.rb
@@ -10,8 +10,9 @@ class GarbageCollectBlobsJob
   # See the note on: https://github.com/rails/rails/blob/master/activestorage/app/models/active_storage/attachment.rb#L5-L8
 
   def perform
-    orphan_query = 'SELECT * FROM active_storage_blobs asb WHERE asb.id NOT IN '\
-                   '(SELECT distinct blob_id FROM active_storage_attachments)'
+    orphan_query = 'SELECT * FROM active_storage_blobs WHERE active_storage_blobs.id IN '\
+                   '( SELECT id FROM active_storage_blobs asb EXCEPT SELECT DISTINCT blob_id '\
+                   'FROM active_storage_attachments asa INNER JOIN active_storage_blobs asb ON asb.id=asa.blob_id)'
     orphan_blobs = ActiveStorage::Blob.find_by_sql(orphan_query)
     orphan_blobs.each(&:purge)
   end
