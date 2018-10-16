3704994df30b5c3a40f91430c209650312457f0f
Weiwei Shi <weiwei.shi@ualberta.ca>
2018-10-15 23:51:46 -0600

    remove redundant part of the query, adding notes, and scale down the frequency of the job

--- app/jobs/garbage_collect_blobs_job.rb
+++ app/jobs/garbage_collect_blobs_job.rb
@@ -9,10 +9,17 @@ class GarbageCollectBlobsJob
   # Therefore we need a process for removing blobs that become orphaned over time as all of their associated attachments cease to exist.
   # See the note on: https://github.com/rails/rails/blob/master/activestorage/app/models/active_storage/attachment.rb#L5-L8
 
+  # This query is to get any ActiveStorage::Blob ids that are not in the ActiveStorage::Attachment table - meaning that the blob was orphaned as there is no more association found within the attachment table.
+  # This is equivalent to:
+  # ActiveStorage::Blob.where([
+  # "id NOT IN (?)",
+  # ActiveStorage::Attachment.pluck("blob_id")
+  # )]
+  # but faster.
+
   def perform
-    orphan_query = 'SELECT * FROM active_storage_blobs WHERE active_storage_blobs.id IN '\
-                   '( SELECT id FROM active_storage_blobs asb EXCEPT SELECT DISTINCT blob_id '\
-                   'FROM active_storage_attachments asa INNER JOIN active_storage_blobs asb ON asb.id=asa.blob_id)'
+    orphan_query = 'SELECT id FROM active_storage_blobs asb EXCEPT SELECT DISTINCT blob_id '\
+                   'FROM active_storage_attachments asa INNER JOIN active_storage_blobs asb ON asb.id=asa.blob_id'
     orphan_blobs = ActiveStorage::Blob.find_by_sql(orphan_query)
     orphan_blobs.each(&:purge)
   end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,7 @@ Sidekiq.configure_server do |config|
   config.redis = { url: Rails.application.secrets.redis_url }
 
   schedule_file = 'config/schedule.yml'
-  if File.exist?(schedule_file) && Rails.application.secrets.run_scheduled_jobs
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) 
-  end
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
 end
 
 Sidekiq.configure_client do |config|
