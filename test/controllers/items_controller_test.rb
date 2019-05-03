d502d8a068aa4bab94f09388004fc6c52941e2b8
ConnorSheremeta <sheremet@ualberta.ca>
2019-05-03 10:27:08 -0600

    Added tests for viewing draft objects in profile, not being able to edit an item if not owned/logged in, and not being able to view/download an authorized item if not authorized

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -39,16 +39,18 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
       unlocked_thesis.save!
     end
 
+    @regular_user = users(:regular)
     @admin = users(:admin)
-    sign_in_as @admin
   end
 
   test 'should be able to show an item' do
+    sign_in_as @admin
     get item_url(@item)
     assert_response :success
   end
 
   test 'should be able to edit an item' do
+    sign_in_as @admin
     # test editing of an item.
     get edit_item_url(@item)
     draft_item = DraftItem.find_by(uuid: @item.id)
@@ -60,7 +62,21 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to admin_thesis_draft_path(id: Wicked::FIRST_STEP, thesis_id: draft_thesis.id)
   end
 
+  test 'shouldnt be able to edit an item if not owned' do
+    sign_in_as @regular_user
+    get edit_item_url(@item)
+    assert_redirected_to root_url
+    assert_equal I18n.t('authorization.user_not_authorized'), flash[:alert]
+  end
+
+  test 'shouldnt be able to edit an item if not logged in' do
+    get edit_item_url(@item)
+    assert_redirected_to root_url
+    assert_equal I18n.t('authorization.user_not_authorized_try_logging_in'), flash[:alert]
+  end
+
   test 'should be able to increment and fetch view count' do
+    sign_in_as @admin
     assert_equal [0, 0], Statistics.for(item_id: @item.id)
     get item_url(@item)
     assert_equal [1, 0], Statistics.for(item_id: @item.id)
--- test/controllers/profile_controller_test.rb
+++ test/controllers/profile_controller_test.rb
@@ -2,10 +2,69 @@ require 'test_helper'
 
 class ProfileControllerTest < ActionDispatch::IntegrationTest
 
+  def before_all
+    super
+    @community = locked_ldp_fixture(Community, :books).unlock_and_fetch_ldp_object(&:save!)
+    @item_collection = locked_ldp_fixture(Collection, :books).unlock_and_fetch_ldp_object(&:save!)
+    @theses_collection = Collection.new_locked_ldp_object(title: 'Thesis collection',
+                                                          owner: 1,
+                                                          restricted: true,
+                                                          community_id: @community.id)
+                                   .unlock_and_fetch_ldp_object(&:save!)
+  end
+
+  setup do
+    @admin = users(:admin)
+    @user = users(:regular)
+  end
+
   test 'should get profile index' do
-    sign_in_as(users(:admin))
+    sign_in_as(@admin)
     get profile_url
     assert_response :success
+  end
+
+  test 'should view draft item in profile' do
+    sign_in_as(@user)
+
+    draft_item = draft_items(:inactive)
+
+    patch item_draft_url(id: :describe_item, item_id: draft_item.id), params: {
+      draft_item: {
+        title: 'Random Book',
+        type_id: types(:book).id,
+        language_ids: [languages(:english).id],
+        creators: ['Jane Doe', 'Bob Smith'],
+        subjects: ['Best Seller', 'Adventure'],
+        date_created: Date.current,
+        description: 'Really random description about this random book',
+        community_id: [@community.id],
+        collection_id: [@item_collection.id]
+      }
+    }
+    # debugger
+    get profile_url
+    assert_includes @response.body, 'Random Book'
+  end
+
+  test 'should view draft thesis in profile' do
+    sign_in_as(@admin)
+
+    draft_thesis = draft_theses(:inactive)
+    patch admin_thesis_draft_url(id: :describe_thesis, thesis_id: draft_thesis.id), params: {
+      draft_thesis: {
+        title: 'Random Thesis',
+        creator: 'Jane Doe',
+        graduation_year: 2018,
+        graduation_term: '06',
+        description: 'Really random description about this random thesis',
+        community_id: [@community.id],
+        collection_id: [@theses_collection.id]
+      }
+    }
+
+    get profile_url
+    assert_includes @response.body, 'Random Thesis'
   end
 
 end
