5a947cd9bc846e9e0d8bef50acca7e9d66acfc9c
Matt Barnett <matt@sixtyodd.com>
2019-06-12 15:58:09 -0600

    correct some rubocop issues

--- app/models/jupiter_core/solr_client.rb
+++ app/models/jupiter_core/solr_client.rb
@@ -1,9 +1,12 @@
 class JupiterCore::SolrClient
+
   include Singleton
 
-  SOLR_CONFIG = YAML.safe_load(ERB.new(File.read("#{Rails.root}/config/solr.yml")).result, [], [], true)[Rails.env].symbolize_keys
+  SOLR_CONFIG = YAML.safe_load(ERB.new(File.read(Rails.root.join('config', 'solr.yml'))).result,
+                               [], [], true)[Rails.env].symbolize_keys
 
   def connection
-    @solr_connection ||= RSolr.connect :url => SOLR_CONFIG[:url]
+    @connection ||= RSolr.connect url: SOLR_CONFIG[:url]
   end
+
 end
