339bd4f8a1adc1055d8532c54a5827ac7424396e
Matt Barnett <matt@sixtyodd.com>
2019-08-28 18:32:11 -0600

    Rubocop cleanup

--- app/models/ar_community.rb
+++ app/models/ar_community.rb
@@ -6,14 +6,16 @@ class ArCommunity < ApplicationRecord
   end
 
   def self.from_community(community)
-    raise ArgumentError, "Community #{community.id} already migrated to ActiveRecord" if ArCommunity.find_by(id: community.id) != nil
+    raise ArgumentError, "Community #{community.id} already migrated" if ArCommunity.find_by(id: community.id).present?
 
     ar_community = ArCommunity.new(id: community.id)
 
     # this is named differently in ActiveFedora
     ar_community.owner_id = community.owner
 
-    attributes = ar_community.attributes.keys.reject {|k| k == 'owner_id' || k == 'created_at' || k == 'updated_at' || k == 'logo_id'}
+    attributes = ar_community.attributes.keys.reject do |k|
+      ['owner_id', 'created_at', 'updated_at', 'logo_id'].include?(k)
+    end
 
     attributes.each do |attr|
       ar_community.send("#{attr}=", community.send(attr))
@@ -24,12 +26,15 @@ class ArCommunity < ApplicationRecord
 
     # add an association between the same underlying blob the Community uses and the new ActiveRecord version
     if community.logo_attachment.present?
-      new_attachment = ActiveStorage::Attachment.create(record: ar_community, blob: community.logo_attachment.blob, name: :logo)
+      new_attachment = ActiveStorage::Attachment.create(record: ar_community,
+                                                        blob: community.logo_attachment.blob,
+                                                        name: :logo)
       # because of the uuid id column, the record_id on new_attachment (currently of type integer), is broken
       # but that's ok. we're going to fix that with this data
       new_attachment.upcoming_record_id = ar_community.id
       new_attachment.save!
     end
+    ar_community
   end
 
 end
