a335a999d682d2f0cb502ccff64cbd750e73e7e1
Connor Sheremeta <sheremet@ualberta.ca>
2019-09-12 11:53:20 -0600

    Improvement on rack-attack configuration (#1265)
    - Add logging to log and rollbar when an IP is blocked
    - Protect home, search, and auth/saml routes by blocking at 60 hits/minute
    - Safelist IPs based off of a comma separated environment variable

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,18 +1,31 @@
 class Rack::Attack
 
-  ### Throttle Spammy Clients ###
-  #
-  # If any single client IP is making tons of requests, then they're
-  # probably malicious or a poorly-configured scraper. Either way, they
-  # don't deserve to hog all of the app server's CPU. Cut them off!
-  #
-  # Note: If you're serving assets through rack, those requests may be
-  # counted by rack-attack and this throttle may be activated too
-  # quickly. If so, enable the condition to exclude them from tracking.
-  #
-  # Throttle all requests by IP (60rpm)
-  #
-  # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  throttle('req/ip', limit: 300, period: 5.minutes, &:ip)
+  class Request < ::Rack::Request
 
+    def allowed_ip?
+      allowed_ips = []
+      if Rails.application.secrets.rack_attack_safelisted_ips.present?
+        allowed_ips = Rails.application.secrets.rack_attack_safelisted_ips.split(',')
+      end
+
+      allowed_ips.include?(ip)
+    end
+
+  end
+
+  safelist('allow safelisted ips', &:allowed_ip?)
+
+  throttle('req/ip', limit: 60, period: 1.minute) do |req|
+    req.ip if ['/search', '/', '/auth/saml'].include?(req.path)
+  end
+
+end
+
+# Log blocked events
+ActiveSupport::Notifications.subscribe('rack.attack') do |_, _, _, _, payload|
+  req = payload[:request]
+  if req.env['rack.attack.match_type'] == :throttle
+    Rails.logger.info "[Rack::Attack][Blocked] ip: \"#{req.ip}\", path: \"#{req.path}\""
+    Rollbar.info("[Rack::Attack][Blocked] ip: \"#{req.ip}\", path: \"#{req.path}\"")
+  end
 end
