fa44471da977863283ad9b6179ba23525f95c373
Matt Barnett <matt@sixtyodd.com>
2019-10-21 16:24:21 -0600

    refix migrations post rebase

--- db/migrate/20190806193857_add_ar_collection.rb
+++ db/migrate/20190806193857_add_ar_collection.rb
@@ -1,21 +1,19 @@
+
 class AddArCollection < ActiveRecord::Migration[5.2]
   def change
-    create_table :ar_collections do |t|
-      t.uuid :collection_id
-      t.integer :visibility, default: 0, null: false
+    create_table :ar_collections, id: :uuid,  default: 'uuid_generate_v4()' do |t|
+      t.string :visibility
       t.references :owner, null: false, index: true, foreign_key: {to_table: :users, column: :id}
       t.datetime :record_created_at
       t.string :hydra_noid
       t.datetime :date_ingested
       t.string :title
       t.string :fedora3_uuid
-      t.references :depositor, index: true, foreign_key: {to_table: :users, column: :id}
+      t.string :depositor
       t.uuid :community_id
       t.text :description
       t.json :creators, array: true
       t.boolean :restricted, default: false, null: false
-
-
       t.timestamps
     end
   end
--- db/migrate/20190814210221_add_is_era_published_column.rb
+++ db/migrate/20190814210221_add_is_era_published_column.rb
@@ -1,8 +1,7 @@
+
 class AddIsEraPublishedColumn < ActiveRecord::Migration[5.2]
   def change
     add_column :draft_items, :is_published_in_era, :boolean, default: false, index: true
     add_column :draft_theses, :is_published_in_era, :boolean, default: false, index: true
-    add_column :ar_collections, :is_published_in_era, :boolean, default: false, index: true
-    add_column :ar_communities, :is_published_in_era, :boolean, default: false, index: true
   end
 end
