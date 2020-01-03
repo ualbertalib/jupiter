1d40c026352c5f9b8395bc1dc26fef54c857a0e3
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2020-01-03 10:58:33 -0700

    Rails/FilePath default changed to slashes
    In the gem bump for rubocop-rails to 2.4.1 the default EnforcementStyle
    changed from arguments to slashes.  That means that
    `Rails.root.join('config', 'controlled_vocabularies', '*.yml')` becomes
    `Rails.root.join('config/controlled_vocabularies/*.yml')`.  It was
    [previously thought](https://github.com/rubocop-hq/rubocop-rails/pull/128#issuecomment-530671304)
    that the first way was OS agnostic but it turns out that Rails.root is a
    Pathname instance which accounts for this.  Now the slash is prefered for
    fewer strings/arguments.

--- app/models/jupiter_core/solr_services/client.rb
+++ app/models/jupiter_core/solr_services/client.rb
@@ -2,7 +2,7 @@ class JupiterCore::SolrServices::Client
 
   include Singleton
 
-  SOLR_CONFIG = YAML.safe_load(ERB.new(File.read(Rails.root.join('config', 'solr.yml'))).result,
+  SOLR_CONFIG = YAML.safe_load(ERB.new(File.read(Rails.root.join('config/solr.yml'))).result,
                                [], [], true)[Rails.env].symbolize_keys
 
   def connection
