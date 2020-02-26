6097e3d08b0eab36bef7c42b20c51e6631796a0f
Shane Murnaghan <murnagha@gmail.com>
2020-02-26 02:08:26 -0700

    Fix broken tests, cant blindly convert everything to use arrow functions

--- app/javascript/src/application/toggle_collections.js
+++ app/javascript/src/application/toggle_collections.js
@@ -1,10 +1,12 @@
-document.addEventListener('turbolinks:load', () => {
-  $('button.js-close-btn').click((event) => {
-    const communityId = $(this).data('communityId');
+function hideCollections(event) {
+  const communityId = $(this).data('communityId');
 
-    $(this).hide();
-    $(`.js-collection-btn[data-community-id='${communityId}']`).show();
-    $(`ul#${communityId}`).hide();
-    event.preventDefault();
-  });
+  $(this).hide();
+  $(`.js-collection-btn[data-community-id='${communityId}']`).show();
+  $(`ul#${communityId}`).hide();
+  event.preventDefault();
+}
+
+document.addEventListener('turbolinks:load', () => {
+  $('button.js-close-btn').click(hideCollections);
 });
