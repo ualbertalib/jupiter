227897acfafc1d8012b5cfd740c58c87394ffe56
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2020-03-06 11:52:07 -0700

    Add safe migration for system account
    As we add a system user from a migration we need to assume the user may
    already exist. This approch will provide a new/existing user a password
    the application is expectin. On a migration removal we don't do anything
    in case the system user account was in fact there.

--- db/migrate/20200305210657_add_system_account_if_needed.rb
+++ db/migrate/20200305210657_add_system_account_if_needed.rb
@@ -1,17 +1,16 @@
 class AddSystemAccountIfNeeded < ActiveRecord::Migration[6.0]
   def up    
 
-    unless User.find_by(email: 'ditech@ualberta.ca').blank?
-      User.create ({
-        name: 'System user',
-        email: 'ditech@ualberta.ca',
-        admin: false,
-        password_digest: BCrypt::Password.create(
-          Rails.application.secrets.system_user_password
-        )
-      })
-    end
+    user = User.find_or_create_by email: 'ditech@ualberta.ca'
+    user.name ||= 'System user'
+    user.admin = false
+    user.password_digest = BCrypt::Password.create(
+      Rails.application.secrets.system_user_password
+    )
+    user.save
   end
-
-  def down; end
+  # We are not defining a down method because we are assuming that our system
+  # user may exist before our migration. Our safest approach is to provide a 
+  # password that the system expects and leave the rest the same without 
+  # compromising the security of the system user.
 end
