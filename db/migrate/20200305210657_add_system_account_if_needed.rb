4d62e327ddbb2f0576eb1f49059fa56f7c27d907
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2020-03-09 10:53:07 -0600

    Add changes to Gemfile.lock and minor change

--- db/migrate/20200305210657_add_system_account_if_needed.rb
+++ db/migrate/20200305210657_add_system_account_if_needed.rb
@@ -1,6 +1,6 @@
 class AddSystemAccountIfNeeded < ActiveRecord::Migration[6.0]
-  def up    
 
+  def up
     user = User.find_or_create_by email: 'ditech@ualberta.ca'
     user.name ||= 'System user'
     user.admin = false
@@ -10,7 +10,8 @@ class AddSystemAccountIfNeeded < ActiveRecord::Migration[6.0]
     user.save
   end
   # We are not defining a down method because we are assuming that our system
-  # user may exist before our migration. Our safest approach is to provide a 
-  # password that the system expects and leave the rest the same without 
+  # user may exist before our migration. Our safest approach is to provide a
+  # password that the system expects and leave the rest the same without
   # compromising the security of the system user.
+
 end
