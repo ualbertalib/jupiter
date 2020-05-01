3b15b7dad7e13744d9fa08c75950c3cbd4c6d7cd
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2020-05-01 14:37:55 -0600

    address pr feedback
    - better path handling of files
    - use find_each to batch which would reduce memory use
    - added headings to the report log

--- db/migrate/20200430205827_deconcatenate_subject_in_item_and_thesis.rb
+++ db/migrate/20200430205827_deconcatenate_subject_in_item_and_thesis.rb
@@ -4,18 +4,30 @@ class DeconcatenateSubjectInItemAndThesis < ActiveRecord::Migration[6.0]
     add_column :items, :deconcatenated_subject, :json
     add_column :theses, :deconcatenated_subject, :json
 
-    concatenated_subject_ids = File.readlines('db/migrate/ERA_subject_issue.txt').collect(&:strip)
-    report = Logger.new('log/concatenatedsubjects.log')
-    (Item.all + Thesis.all).each do |item|
-      item.deconcatenated_subject = if concatenated_subject_ids.include? item.id
-                                      deconcatenated_subject = item.subject.map { |subject| subject.split(%r{,\s*}) }.flatten
-                                      report.info("#{item.id}, \"#{item.title}\", #{item.subject}, #{deconcatenated_subject}")
-                                      deconcatenated_subject
-                                    else
-                                      item.subject
-                                    end
+    @concatenated_subject_ids = File.readlines(Rails.root.join('ERA_subject_issue.txt')).collect(&:strip)
+    @report = Logger.new(Rails.root.join('log/concatenated_subjects.log'))
+
+    Item.find_each do |item|
+      item.deconcatenated_subject = deconcatenated_subject(item)
       item.save!
     end
+
+    Thesis.find_each do |item|
+      item.deconcatenated_subject = deconcatenated_subject(item)
+      item.save!
+    end
+  end
+
+  def deconcatenated_subject(item)
+    return item.subject unless @concatenated_subject_ids.include? item.id
+
+    deconcatenated_subject = item.subject.map { |subject| subject.split(%r{,\s*}) }.flatten
+    @report.info(
+      "id: #{item.id}, title: \"#{item.title}\", subject: #{item.subject},"\
+      " deconcatenated subject: #{deconcatenated_subject}"
+    )
+
+    deconcatenated_subject
   end
 
 end
