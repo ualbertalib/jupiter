f16f6fe7e75dade8fd0eff3db1b63dfc2bc4d445
pgwillia <tricia.g.jenkins@gmail.com>
2020-07-14 15:04:39 -0600

    add migration to fix concatenated subjects (part 1) (#1627)
    This will take a subject like ["Cat, Dog, Rabbit"] and make it ["Cat", "Dog", "Rabbit"] in a new deconcatenated_subject column.
    
    We're doing this in two parts to make data cleanup a little more sane than edits in production.
    
    This assumes that at some point in the future this new column will be reviewed and replace the Item's subject.
    
    We want to wrap this in the ability to catch an exception if the file doesn't exist, and then not do the rest of the migration and carry on. That way we can delete the file ERA_subject_issue.txt once this is all fixed instead of having to keep it around forever to avoid breaking migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_061736) do
+ActiveRecord::Schema.define(version: 2020_04_30_205827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -229,6 +229,7 @@ ActiveRecord::Schema.define(version: 2020_04_28_061736) do
     t.string "aasm_state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "deconcatenated_subject", array: true
     t.index ["logo_id"], name: "index_items_on_logo_id"
     t.index ["owner_id"], name: "index_items_on_owner_id"
   end
@@ -289,6 +290,7 @@ ActiveRecord::Schema.define(version: 2020_04_28_061736) do
     t.string "aasm_state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "deconcatenated_subject", array: true
     t.index ["logo_id"], name: "index_theses_on_logo_id"
     t.index ["owner_id"], name: "index_theses_on_owner_id"
   end
