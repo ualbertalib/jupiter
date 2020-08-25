079230bb1a040880ad74bbcf59c7a8ec9ac3cd23
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2020-08-25 15:22:52 -0600

    use port 443 for ssl and the proper headers
    https://stackoverflow.com/a/61593945
    
    Was getting 433 errors in the browser and web logs:
    web_1       | [793783cc-d5c6-4e99-b70d-d9957b7e9170] ActionController::InvalidAuthenticityToken (ActionController::InvalidAuthenticityToken):
    web_1       | [793783cc-d5c6-4e99-b70d-d9957b7e9170]
    web_1       | [793783cc-d5c6-4e99-b70d-d9957b7e9170] omniauth-rails_csrf_protection (0.1.2) lib/omniauth/rails_csrf_protection/token_verifier.rb:34:in `call'

--- config/environments/uat.rb
+++ config/environments/uat.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.default_url_options = { host: 'uat.library.ualberta.ca', port: 80, protocol: 'http' }
+Rails.application.routes.default_url_options = { host: 'uat.library.ualberta.ca', port: 443, protocol: 'https' }
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
