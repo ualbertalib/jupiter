0dae3f4405ddb2e8692ec8e4a1ff064ef7aaa214
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2020-11-26 10:56:54 -0700

    remove unused abstractions
    Rails scaffolding assumes full CRUD which isn't needed for the Peel Redirects functionality.  In the interest of avoid premature optimization, I'll simplify this even further until its actually needed.

--- app/controllers/digitization/images_controller.rb
+++ app/controllers/digitization/images_controller.rb
@@ -1,20 +1,8 @@
 class Digitization::ImagesController < ApplicationController
 
-  before_action :set_digitization_image, only: [:show]
-
-  def show; end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_digitization_image
+  def show
     @digitization_image = Digitization::Image.find(params[:id])
     authorize @digitization_image
-  end
-
-  # Only allow a list of trusted parameters through.
-  def digitization_image_params
-    params.require(:digitization_image).permit(:peel_image_id)
   end
 
 end
--- app/controllers/digitization/newspapers_controller.rb
+++ app/controllers/digitization/newspapers_controller.rb
@@ -1,20 +1,8 @@
 class Digitization::NewspapersController < ApplicationController
 
-  before_action :set_digitization_newspaper, only: [:show]
-
-  def show; end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_digitization_newspaper
+  def show
     @digitization_newspaper = Digitization::Newspaper.find(params[:id])
     authorize @digitization_newspaper
-  end
-
-  # Only allow a list of trusted parameters through.
-  def digitization_newspaper_params
-    params.require(:digitization_newspaper).permit(:publication_code, :year, :month, :day)
   end
 
 end
