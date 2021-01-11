37c8f7ba1eccc94776b2136bb0a1a59399b05c66
Connor Sheremeta <sheremet@ualberta.ca>
2021-01-11 11:24:41 -0700

    Add tests for deferred_simple_solr_query - Note: deferred_simple_solr_query doesnt work in the test environnment so all these tests will fail at the moment

---
+++ test/models/deferred_simple_solr_query_test.rb
@@ -0,0 +1,237 @@
+require 'test_helper'
+
+class DeferredSimpleSolrQueryTest < ActiveSupport::TestCase
+
+  test 'check item and thesis' do
+    deferred_item_query = Item.solr_query.where({})
+    deferred_thesis_query = Thesis.solr_query.where({})
+    deferred_query = deferred_item_query + deferred_thesis_query
+    assert_equal Item.count, deferred_item_query.total_count
+    assert_equal Thesis.count, deferred_thesis_query.total_count
+    assert_equal (Item.count + Thesis.count), deferred_query.total_count
+
+    deferred_item_results = []
+    deferred_item_query.each do |item|
+      deferred_item_results << item
+    end
+    # Ensure results match.
+    assert deferred_item_results.sort == Item.all.sort
+
+    deferred_thesis_results = []
+    deferred_thesis_query.each do |thesis|
+      deferred_thesis_results << thesis
+    end
+    # Ensure results match.
+    assert deferred_thesis_results.sort == Thesis.all.sort
+
+    deferred_results = []
+    deferred_query.each do |obj|
+      deferred_results << obj
+    end
+    # Ensure results match.
+    all_objects = Item.all + Thesis.all
+    (deferred_results - all_objects).blank? and (all_objects - deferred_results).blank?
+  end
+
+  test 'check item and thesis with collection/community' do
+    item = Item.first
+    path = item.member_of_paths.first
+
+    deferred_item_query = Item.solr_query.where(member_of_paths: path)
+    deferred_thesis_query = Thesis.solr_query.where(member_of_paths: path)
+    deferred_query = deferred_item_query + deferred_thesis_query
+
+    collections = []
+    item.each_community_collection do |_, collection|
+      collections << collection
+    end
+    collection = collections.first
+
+    member_items = collection.member_items
+    member_theses = collection.member_theses
+    member_objects = collection.member_objects
+    assert_equal member_items.count, deferred_item_query.total_count
+    assert_equal member_theses.count, deferred_thesis_query.total_count
+    assert_equal member_objects.count, deferred_query.total_count
+
+    deferred_item_results = []
+    deferred_item_query.each do |item_result|
+      deferred_item_results << item_result
+    end
+    # Ensure results match.
+    assert_equal deferred_item_results.sort, member_items.sort
+
+    deferred_thesis_results = []
+    deferred_thesis_query.each do |thesis|
+      deferred_thesis_results << thesis
+    end
+    # Ensure results match.
+    assert_equal deferred_thesis_results.sort, member_theses.sort
+
+    deferred_results = []
+    deferred_query.each do |obj|
+      deferred_results << obj
+    end
+    # Ensure results match.
+    assert (deferred_results - member_objects).blank? and (member_objects - deferred_results).blank?
+  end
+
+  test 'check item and thesis with two different collection/community' do
+    item = Item.first
+    item_path = item.member_of_paths.first
+    thesis = Thesis.where.not('member_of_paths::text LIKE ?', "%#{item_path}%").first
+    thesis_path = thesis.member_of_paths.first
+
+    deferred_item_query = Item.solr_query.where(member_of_paths: item_path)
+    deferred_thesis_query = Thesis.solr_query.where(member_of_paths: thesis_path)
+    deferred_query = deferred_item_query + deferred_thesis_query
+
+    item_collections = []
+    item.each_community_collection do |_, collection|
+      item_collections << collection
+    end
+    item_collection = item_collections.first
+
+    thesis_collections = []
+    thesis.each_community_collection do |_, collection|
+      thesis_collections << collection
+    end
+    thesis_collection = thesis_collections.first
+
+    member_items = item_collection.member_items
+    member_theses = thesis_collection.member_theses
+    member_objects = member_items + member_theses
+
+    assert_equal member_items.count, deferred_item_query.total_count
+    assert_equal member_theses.count, deferred_thesis_query.total_count
+    assert_equal member_objects.count, deferred_query.total_count
+
+    deferred_item_results = []
+    deferred_item_query.each do |item_result|
+      deferred_item_results << item_result
+    end
+    # Ensure results match.
+    assert_equal deferred_item_results.sort, member_items.sort
+
+    deferred_thesis_results = []
+    deferred_thesis_query.each do |thesis_result|
+      deferred_thesis_results << thesis_result
+    end
+    # Ensure results match.
+    assert_equal deferred_thesis_results.sort, member_theses.sort
+
+    deferred_results = []
+    deferred_query.each do |obj|
+      deferred_results << obj
+    end
+    # Ensure results match.
+    assert (deferred_results - member_objects).blank? and (member_objects - deferred_results).blank?
+  end
+
+  test 'check item and thesis with collection/community for a user' do
+    user = User.first
+    item = Item.first
+    path = item.member_of_paths.first
+
+    deferred_item_query = Item.solr_query.where(member_of_paths: path)
+    deferred_thesis_query = Thesis.solr_query.where(member_of_paths: path)
+    deferred_query = (deferred_item_query + deferred_thesis_query).where(owner: user.id)
+
+    collections = []
+    item.each_community_collection do |_, collection|
+      collections << collection
+    end
+    collection = collections.first
+
+    member_items = collection.member_items.select do |member_item|
+      member_item.owner_id == user.id
+    end
+
+    member_theses = collection.member_theses.select do |member_thesis|
+      member_thesis.owner_id == user.id
+    end
+
+    member_objects = collection.member_objects.select do |member_object|
+      member_object.owner_id == user.id
+    end
+
+    # This next one fails because deferred result returns one more than it should.
+    # assert_equal member_items.count, deferred_item_query.total_count
+    assert_equal member_theses.count, deferred_thesis_query.total_count
+    assert_equal member_objects.count, deferred_query.total_count
+
+    deferred_item_results = []
+    deferred_item_query.each do |item_result|
+      deferred_item_results << item_result
+    end
+    # Ensure results match.
+    assert_equal deferred_item_results.sort, member_items.sort
+
+    deferred_thesis_results = []
+    deferred_thesis_query.each do |thesis|
+      deferred_thesis_results << thesis
+    end
+    # Ensure results match.
+    assert_equal deferred_thesis_results.sort, member_theses.sort
+
+    deferred_results = []
+    deferred_query.each do |obj|
+      deferred_results << obj
+    end
+    # Ensure results match.
+    assert (deferred_results - member_objects).blank? and (member_objects - deferred_results).blank?
+  end
+
+  test 'check all items belonging to a certain path and user' do
+    user = User.first
+    item = Item.where(owner_id: user.id).first
+    path = item.member_of_paths.first
+
+    deferred_item_query = Item.solr_query.where({})
+    deferred_thesis_query = Thesis.solr_query.where({})
+    deferred_query_with_path = (deferred_item_query + deferred_thesis_query).where(member_of_paths: path)
+    deferred_query = deferred_query_with_path.where(owner: user.id)
+
+    collections = []
+    item.each_community_collection do |_, collection|
+      collections << collection
+    end
+    collection = collections.first
+
+    member_objects = collection.member_objects.select do |member_object|
+      member_object.owner_id == user.id
+    end
+
+    assert_equal member_objects.count, deferred_query.total_count
+
+    deferred_results = []
+    deferred_query.each do |obj|
+      deferred_results << obj
+    end
+    # Ensure results match.
+    assert (deferred_results - member_objects).blank? and (member_objects - deferred_results).blank?
+  end
+
+  test 'check all items belonging to two users' do
+    user1 = User.first
+    user2 = User.second
+
+    deferred_query1 = Item.solr_query.where(owner_id: user1.id)
+    deferred_query2 = Item.solr_query.where(owner_id: user2.id)
+    deferred_query = deferred_query1 + deferred_query2
+
+    items_by_users = Item.where('owner_id = ? OR owner_id = ?', user1.id, user2.id)
+
+    # This fails. deferred_query.total_count gives: undefined method `first' for nil:NilClass
+    # assert_equal items_owned.count, deferred_query.total_count
+
+    # This (deferred_query.each) also fails for the same reason as above.
+    # deferred_results = []
+    # deferred_query.each do |obj|
+    #   deferred_results << obj
+    # end
+    # Ensure results match.
+    # assert (items_by_users - items_owned).blank? and (items_owned - items_by_users).blank?
+  end
+
+end
