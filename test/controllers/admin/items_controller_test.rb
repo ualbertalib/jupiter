339eb32d4ba2821333370025f8b12d76677a6a31
Connor Sheremeta <sheremet@ualberta.ca>
2021-01-11 15:14:07 -0700

    fix flapping test

--- test/controllers/admin/items_controller_test.rb
+++ test/controllers/admin/items_controller_test.rb
@@ -29,8 +29,9 @@ class Admin::ItemsControllerTest < ActionDispatch::IntegrationTest
   test 'reset DOI should queue job' do
     assert_no_enqueued_jobs only: DOIRemoveJob
     patch reset_doi_admin_item_url(@item)
-    assert_enqueued_jobs 1, only: DOICreateJob
     assert_response :redirect
+    assert_redirected_to root_url
+    assert_enqueued_jobs 1, only: DOICreateJob
 
     clear_enqueued_jobs
   end
