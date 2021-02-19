6d040e2fe9f531f7541e6a17ef2de1de7869a624
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2021-02-19 14:33:38 -0700

    Connected db for i18n active record initializer
    Initializers are invoked by `rails db:create db:schema:load` in our github actions 'Run Tests'.  Since the database hasn't been created yet the initializer will fail on the check where the translation table exists?  This will ensure that the whole thing is skipped if the database hasn't been created.

--- config/initializers/i18n_active_record.rb
+++ config/initializers/i18n_active_record.rb
@@ -2,7 +2,7 @@ require 'i18n/backend/active_record'
 
 Translation = I18n::Backend::ActiveRecord::Translation
 
-if Translation.table_exists?
+if ActiveRecord::Base.connected? && Translation.table_exists?
   I18n.backend = I18n::Backend::ActiveRecord.new
 
   I18n::Backend::ActiveRecord.include I18n::Backend::Memoize
