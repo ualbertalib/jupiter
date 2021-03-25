79eecc0a4d53f93c8993e606bdd4eebd94e968ec
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2021-03-25 17:40:49 -0600

    delegate arel for search helper

--- app/decorators/paginating_decorator.rb
+++ app/decorators/paginating_decorator.rb
@@ -1,5 +1,5 @@
 class PaginatingDecorator < Draper::CollectionDecorator
 
-  delegate :current_page, :total_pages, :limit_value, :entry_name, :total_count, :offset_value, :last_page?
+  delegate :arel, :current_page, :total_pages, :limit_value, :entry_name, :total_count, :offset_value, :last_page?
 
 end
