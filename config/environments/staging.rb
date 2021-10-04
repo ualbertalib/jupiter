feaebde78f50691b54c831460903490f1a9c45d9
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2021-10-04 15:44:16 -0600

    rubocop nit

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,5 @@
-Rails.application.routes.default_url_options = { host: Rails.application.secrets.era_host, port: 443, protocol: 'https' }
+Rails.application.routes.default_url_options = { host: Rails.application.secrets.era_host, port: 443,
+                                                 protocol: 'https' }
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
