c50fe1af3bdb611810f16c6319b4228a16567ddc
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2021-11-05 16:08:48 -0600

    Save an extra call to the DB
    Prefer this to batch_ingest.books.new because if the book validation fails then the batch_ingest status cannot be updated.  This became apparent when reporting the status of an ingest.

--- app/jobs/digitization/batch_metadata_ingestion_job.rb
+++ app/jobs/digitization/batch_metadata_ingestion_job.rb
@@ -53,7 +53,9 @@ class Digitization::BatchMetadataIngestionJob < ApplicationJob
 
     graph.query(query_for_local_identifiers) do |statement|
       # Each of the discrete items will become its own object
-      book = Digitization::Book.new(owner_id: batch_ingest.user_id)
+      # prefer this to batch_ingest.books.new because if the book validation fails then the batch_ingest status cannot be updated
+      book = Digitization::Book.new(owner_id: batch_ingest.user_id,
+                                    digitization_batch_metadata_ingest_id: batch_ingest.id)
 
       # Now we want know all about each item
       query_for_this_items_attributes = RDF::Query.new do
@@ -70,7 +72,6 @@ class Digitization::BatchMetadataIngestionJob < ApplicationJob
         end
       end
       book.save!
-      batch_ingest.books << book
     end
   end
 
