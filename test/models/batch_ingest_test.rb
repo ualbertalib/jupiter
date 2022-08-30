89f29acd8d21b07b8e8d4a5c989346f720443b96
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2022-08-30 11:48:11 -0600

    Fix rubocop issues

--- test/models/batch_ingest_test.rb
+++ test/models/batch_ingest_test.rb
@@ -86,8 +86,10 @@ class BatchIngestTest < ActiveSupport::TestCase
                      batch_ingest.errors[:google_spreadsheet_id].second)
         assert_equal('owner_id does not exist in ERA for row 1 of spreadsheet',
                      batch_ingest.errors[:google_spreadsheet_id].third)
-        assert_equal("File(s) 'conference_logo.png, conference.pdf' from row 1 of spreadsheet are not listed in the file list below", 
+        # rubocop:disable Layout/LineLength
+        assert_equal("File(s) 'conference_logo.png, conference.pdf' from row 1 of spreadsheet are not listed in the file list below",
                      batch_ingest.errors[:google_spreadsheet_id].fourth)
+        # rubocop:enable Layout/LineLength
       end
     end
   end
