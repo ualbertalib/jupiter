2a1ea490e0ce3722f3d6e75f29517af897d682f6
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2023-04-20 16:19:38 -0600

    Fixed AIP api collections with nil descriptions
    After running the rake task to enqueue all the collections and communities we received many errors like:
    `ArgumentError: Statement #<RDF::Statement:0x9808(<http://era.ualberta.localhost/aip/v1/collections/a93cbb63-4bb2-4deb-a952-c96c4c851c8c> <http://purl.org/dc/terms/description> nil .)> is incomplete`
    
    274 of our collections have no description.
    
    If there is no description than we can leave out the statement about description.
    
    I checked for which attributes are required, and `description` is the only one here that might be nil https://github.com/ualbertalib/jupiter/blob/18351c928226b3d6883b1d78da24a820374b73d1/db/schema.rb#L91-L107

--- app/controllers/aip/v1/collections_controller.rb
+++ app/controllers/aip/v1/collections_controller.rb
@@ -17,7 +17,6 @@ class Aip::V1::CollectionsController < ApplicationController
       RDF::Statement(subject: self_subject, predicate: RDF.type, object: RDF::Vocab::PCDM.Collection),
       RDF::Statement(subject: self_subject, predicate: RDF::Vocab::PCDM.memberOf, object: @collection.community_id),
       RDF::Statement(subject: self_subject, predicate: RDF::Vocab::DC.title, object: @collection.title),
-      RDF::Statement(subject: self_subject, predicate: RDF::Vocab::DC.description, object: @collection.description),
       RDF::Statement(subject: self_subject, predicate: RDF::Vocab::DC.accessRights, object: @collection.visibility),
       RDF::Statement(subject: self_subject, predicate: TERMS[:ual].restricted_collection,
                      object: @collection.restricted),
@@ -25,6 +24,10 @@ class Aip::V1::CollectionsController < ApplicationController
       RDF::Statement(subject: self_subject, predicate: TERMS[:ual].record_created_in_jupiter,
                      object: @collection.record_created_at)
     ]
+    if @collection.description.present?
+      statements << RDF::Statement(subject: self_subject, predicate: RDF::Vocab::DC.description,
+                                   object: @collection.description)
+    end
 
     rdf_graph_creator.graph.insert(*statements)
 
