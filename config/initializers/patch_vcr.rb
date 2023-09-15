3beca3f1601d91f906513d5dc80f55f5abaf53ed
Shane Murnaghan <1930474+murny@users.noreply.github.com>
2023-09-14 22:07:32 -0600

    Patch v5.0 VCR to fix crashing issue instead of upgrading it

---
+++ config/initializers/patch_vcr.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# NOTE: If we ever upgrade VCR to v6.0+ then this monkeypatch
+# will not be needed anymore and can be safely removed
+# More info: https://github.com/vcr/vcr/pull/907/files
+if Rails.env.test?
+  require 'vcr'
+  require 'vcr/library_hooks/webmock'
+
+  module VCR::LibraryHooks::WebMock
+    def global_hook_disabled?(request)
+      requests = Thread.current[:_vcr_webmock_disabled_requests]
+      requests&.include?(request)
+    end
+
+    def global_hook_disabled_requests
+      Thread.current[:_vcr_webmock_disabled_requests] ||= []
+    end
+  end
+end
