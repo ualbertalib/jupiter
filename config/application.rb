4bb599955b9f4d7f80f7c8a50416481177dec2d6
Shane Murnaghan <1930474+murny@users.noreply.github.com>
2023-10-14 14:06:36 -0600

    Update files from generator

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Jupiter
     require_relative '../lib/jupiter/version'
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
